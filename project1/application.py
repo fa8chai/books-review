7a958a2a07b9f9a32794f12e0894344d9522a9a2
fatima <anne.wi.sw@gmail.com>
2019-10-24 15:17:27 +0300

    h

--- project1/application.py
+++ project1/application.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_bcrypt import Bcrypt
+from sqlalchemy.sql import text
+
 
 app = Flask(__name__)
+
 bcrypt = Bcrypt(app)
+
+
+
+
+app.secret_key= b'\xe8:[\x8cu\xcc\xb7P\x1a\x13\xd0\xa4\xd9\xaci\x89'
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -24,7 +32,10 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if "fname" in session:
+        username = session["fname"]
+
+    return render_template("index.html",name=username)
 
 
 @app.route("/signup",methods=['POST','GET'])
@@ -42,7 +53,46 @@ def signup():
     return render_template('signup.html',title='REGISTER')
 
 
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if db.execute("SELECT * FROM users WHERE email = :email ",{"email":email}).rowcount == 1 :
+    
+            fname = db.execute(text("SELECT fname FROM users WHERE email = :email",{"email":email})).fetchone()
+            session["fname"]= fname
+            return redirect(url_for("index"))
+        return "not req"
+    return render_template("login.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
