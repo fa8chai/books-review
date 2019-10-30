4b94b0574545cab2aa74dbab7a51e6714124706e
fatima <anne.wi.sw@gmail.com>
2019-10-30 17:00:01 +0300

    wtf added

--- project1/application.py
+++ project1/application.py
@@ -1,11 +1,36 @@
 import os
-from flask import Flask, session, render_template, request, redirect, url_for
+from flask import Flask, session, render_template, request, redirect, url_for,jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from flask_bcrypt import Bcrypt
 from sqlalchemy.sql import text
 from datetime import datetime
+import requests
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo,Length
+
+
+class RegistrationForm(FlaskForm):
+    fname = StringField('Username',validators= [DataRequired(),Length(min=3, max=15)])
+    lname = StringField('Full Name', validators=[DataRequired(),Length(min=6, max=25)])
+    email = StringField('Email Address',validators= [DataRequired(),Email(message=('That is not a valid email address.'))])
+    password = PasswordField('New Password',validators= [DataRequired(),Length(min=8, max=25)])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email(message=('That is not a valid email address.'))])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+
+
+
+
 
 app = Flask(__name__)
 
@@ -28,6 +53,39 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route('/api/<isbn>',methods=['GET'])
+def api(isbn):
+    isbn = isbn
+    book = db.execute("SELECT * FROM books WHERE isbn = :isbn",{'isbn' : isbn}).fetchone()
+    db.commit()
+    #print(jsonify(book))
+    if book is None:
+        res = {
+           "error_code": 404,
+           "error_message": "Not Found" 
+        }, 404
+        return jsonify(res)
+    res = db.execute("SELECT  title ,author,year,isbn,COUNT(reviews.id) as review_count ,AVG(reviews.rating) as average_score FROM books  LEFT JOIN reviews ON books.id = book_id WHERE isbn=:isbn GROUP BY title ,author,year,isbn ",{'isbn':isbn}).fetchone()
+    db.commit()
+    print(res)
+   
+    resbook = {
+          "title": res.title,
+          "author": res.author,
+          "year": res.year,
+          "isbn": isbn,
+          "review_count": res.review_count,
+          "average_score": res.average_score
+     }
+    return jsonify(resbook)
+
+
+ 
+ 
+
+
 
 
 @app.route("/")
@@ -69,16 +127,28 @@ def search():
 @app.route('/book/<int:book_id>')
 def book(book_id):
     book = db.execute("SELECT * FROM books WHERE id = :id",{'id' : book_id}).fetchone()
+    db.commit()
     if book is None :
         return render_template("error.html",error="NO BOOK")
     reviews = db.execute("SELECT fname, date, rating, body FROM reviews JOIN users ON user_id = users.id WHERE book_id = :book_id ORDER BY reviews.id DESC ",{'book_id':book_id}).fetchone()
     db.commit()
+    
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "s18kbfQ47SeAy9WFej1gBA", "isbns": book.isbn})
+
+    print(res.json())
+    res = res.json()
+    number_of_ratings = res['books'][0]['work_ratings_count']
+    average_rating = res['books'][0]['average_rating']
+       
+    
+    
+    
     if 'fname' in session:
         print(book)
         username = session["fname"]
-        return render_template('book.html',book=book,reviews=reviews,name=username)
+        return render_template('book.html',book=book,reviews=reviews,name=username,nrating=number_of_ratings,arating=average_rating)
         
-    return render_template('book.html',book=book,reviews=reviews)
+    return render_template('book.html',book=book,reviews=reviews,nrating=number_of_ratings,arating=average_rating ,error="LOGIN FIRST!")
 
 
 
@@ -102,28 +172,37 @@ def review():
         db.execute("INSERT INTO reviews (user_id, book_id, date, body, rating) VALUES (:user_id, :book_id,:date, :body, :rating)",{'user_id':user_id,'book_id':book_id, 'date':date ,'body':body,'rating':rating})
         db.commit()
         return redirect(url_for("book",book_id = book_id))
-    reviews = db.execute("SELECT fname, date, rating, body FROM reviews JOIN users ON user_id = users.id WHERE book_id = :book_id ORDER BY reviews.id DESC ",{'book_id':book_id}).fetchall()
-    db.commit()
-    return render_template('book.html',book=book,reviews=reviews,error="LOGIN FIRST!")
+    return redirect(url_for("book",book_id = book_id))
+
+
+
+
 
 @app.route("/signup",methods=['POST','GET'])
 def signup():
-    if request.method=='POST':
+    form = RegistrationForm()
+    if request.method=='POST' and form.validate_on_submit() :
         fname=request.form.get('fname')
         lname=request.form.get('lname')
         email=request.form.get('email')
         password=request.form.get('password')
         if db.execute("SELECT * FROM users WHERE email = :email ",{"email":email}).rowcount == 1 :
             return render_template('login.html',error="USER ALREADY EXIST!")
+
+        if db.execute("SELECT * FROM users WHERE fname = :fname ",{"fname":fname}).rowcount == 1 :
+            return render_template('signup.html',title='REGISTER',serror="USERNAME ALREADY TAKEN!",form = form)
+
+
         db.execute("INSERT INTO users (fname,lname,email,password) VALUES (:fname,:lname,:email,:password)", {"fname":fname,"lname":lname,"email":email,"password":bcrypt.generate_password_hash(password).decode("utf-8")})
         db.commit()
         session['fname'] = fname
         return redirect(url_for("index"))
-    return render_template('signup.html',title='REGISTER')
+    return render_template('signup.html',title='REGISTER',form = form)
 
 
 @app.route("/login",methods=['GET','POST'])
 def login():
+    form = LoginForm()
     if request.method=='POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -134,11 +213,11 @@ def login():
                   fname = user.fname
                   session['fname'] = fname[0]
                   return redirect(url_for("index"))
-            return render_template("login.html",error="WRONG PASSWORD!!")
-        return render_template('signup.html',title='REGISTER',error="NO USER WITH THAT EMAIL ADDRESS")
+            return render_template("login.html",error="WRONG PASSWORD!!" ,form=form)
+        return render_template('signup.html',title='REGISTER',error=" THERE IS NO USER WITH THAT EMAIL ADDRESS")
 
     
-    return render_template("login.html")
+    return render_template("login.html",form=form)
 
 @app.route('/logout')
 def logout():
